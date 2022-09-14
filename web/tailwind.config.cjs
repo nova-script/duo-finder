19f7a17867eb21e5b897f1c4116d97a3c94f914c
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-14 15:19:19 -0300

    :bulb: Add postcss and tailwindcss

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html'
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        
+      },
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient' : 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%), linear-gradient(0deg, #FFFFFF, #FFFFFF);',  
+        'gamebox-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);'
+      },
+    },
+  },
+  plugins: [],
+}
