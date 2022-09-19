a837fd87dedf5414472463fd262e1f0a8a6201dd
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-19 20:39:23 -0300

    :bulb: Add routes

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,20 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// Screens
+import { Home } from '../screens/Home'
+import { Game } from '../screens/Game'
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes(){
+    return (
+        <Navigator screenOptions={{headerShown: false}}>
+            <Screen
+                name='home'
+                component={Home}/>
+            <Screen
+                name='game'
+                component={Game}/>
+        </Navigator>
+    )
+}
