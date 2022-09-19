7ea1a15896fca0ebdc286a6188a9dd3fd2e599b5
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-09-19 20:39:42 -0300

    :bulb: Add DuoCard component

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,49 @@
+import { TouchableOpacity, View, Text } from 'react-native';
+import { THEME } from '../../theme';
+import { DuoInfo } from '../DuoInfo';
+
+import { GameController } from 'phosphor-react-native';
+
+
+import { styles } from './styles';
+
+export interface DuoCardProps {
+  id: string;
+  hourEnd: string;
+  hourStart: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[]
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+  onConnect: () => void;
+}
+
+export function DuoCard({data, onConnect}: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo label="Nome" value={data.name}/>
+
+      <DuoInfo label="Tempo de Jogo" value={`${data.yearsPlaying} anos`}/>
+
+      <DuoInfo label="Disponibilidade"
+      value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}/>
+      
+      <DuoInfo label="Chamada de áudio?"
+      value={data.useVoiceChannel ? "Sim" : "Não"}
+      colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}/>
+    
+      <TouchableOpacity style={styles.button} onPress={onConnect}>
+        <GameController size={20} color={THEME.COLORS.TEXT}/>
+
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
---
+++ mobile/src/components/DuoCard/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: 200,
+    height: 'auto',
+    alignItems: 'center',
+    backgroundColor: THEME.COLORS.SHAPE,
+    borderRadius: 8,
+    padding: 20,
+    marginRight: 16,
+  },
+  button: {
+    width: '100%',
+    height: 36,
+    borderRadius: 6,
+    backgroundColor: THEME.COLORS.PRIMARY,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonTitle: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    fontSize: THEME.FONT_SIZE.SM,
+    marginLeft: 12,
+  }
+});
